c4b60af526743ad650e2c162477e9cb23563bd3c
jobinjose2233 <43052732+jobinjose2233@users.noreply.github.com>
2018-09-07 11:25:21 +0530

    Add files via upload

---
+++ math calculations.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a=10,b=20,x,y,z,j;
+    x=a+b;
+    y=a-b;
+    z=a*b;
+    j=a/b;
+    System.out.println("a=10,b =20");
+    System.out.println("a+b = "+x);
+    System.out.println("a-b = "+y);
+    System.out.println("a*b = "+z);
+    System.out.println("a/b = "+j);
+  }
+}
